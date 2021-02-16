e059ecd8fb62e1f606f8c4201fc5f62f909cf847
Matheus Kildere <matheuskildery94@gmail.com>
2021-02-16 12:48:04 -0300

    feat: home page

---
+++ lib/app/view/home/components/vehicle_button_component.dart
@@ -0,0 +1,61 @@
+import 'package:app/app/model/home/vehicle_summary.dart';
+import 'package:app/core/enum/Vehicle/vehicle_type.dart';
+import 'package:app/core/utils/theme/colors.dart';
+import 'package:app/core/utils/theme/formatter.dart';
+import 'package:flutter/material.dart';
+
+class VehicleButtonComponent extends StatelessWidget {
+  final VehicleSummary vehicle;
+  final VehicleType type;
+
+  const VehicleButtonComponent({
+    Key key, 
+    this.type,
+    this.vehicle}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      child: Container(
+        height: 233,
+        width: 208,
+        decoration: BoxDecoration(
+          color: type == VehicleType.particular ? grey900 : blueLight,
+          borderRadius: BorderRadius.circular(10)
+        ),
+        child: Padding(
+          padding: EdgeInsets.only(left: 20, right: 20),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Image.network(vehicle.imageUrl,
+                height: 100,
+                width: 185,
+                fit: BoxFit.fitHeight,
+              ),
+              SizedBox(height: 26,),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(vehicle.name,
+                    style: TextStyle(
+                      color: grey,
+                      fontSize: 20
+                    ),
+                  ),
+                  SizedBox(height: 7,),
+                  Text(Formatter.moneySettings(vehicle.value.toString()),
+                    style: TextStyle(
+                      color: grey,
+                      fontSize: 20
+                    ),
+                  )
+                ],
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
