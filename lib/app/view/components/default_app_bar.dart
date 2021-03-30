f291232662c1047c5d8156f57801170aa2ec290f
Brandow Buenos <brandowbuenos@gmail.com>
2021-03-30 15:31:07 -0300

    feat: add back icon option in default_app_bar

--- lib/app/view/components/default_app_bar.dart
+++ lib/app/view/components/default_app_bar.dart
@@ -1,11 +1,13 @@
 import 'package:app/app/view/components/default_top_sheet.dart';
+import 'package:app/core/theme/app_icons.dart';
 import 'package:app/core/theme/colors.dart';
 import 'package:flutter/material.dart';
 
 class DefaultAppBar extends PreferredSize {
   final double size;
+  final bool iconBack;
 
-  DefaultAppBar({this.size});
+  DefaultAppBar({this.size, this.iconBack = false});
 
   @override
   Size get preferredSize => size ?? Size.fromHeight(115);
@@ -22,12 +24,14 @@ class DefaultAppBar extends PreferredSize {
               padding: EdgeInsets.only(left: 33),
               child: InkWell(
                 child: Icon(
-                  Icons.menu,
+                  !iconBack ? Icons.menu : Icons.arrow_back,
                   color: primaryColor,
                   size: 26,
                 ),
                 onTap: () {
-                  DefaultTopSheet.show(context);
+                  !iconBack
+                      ? DefaultTopSheet.show(context)
+                      : Navigator.pop(context);
                 },
               ),
             ),
