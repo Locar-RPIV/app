157473232c748f28d34cd21071fad2e5794dc0de
Matheus Kildere <matheuskildery94@gmail.com>
2021-04-04 21:23:28 -0300

    feat: register cliente

---
+++ lib/core/repository/register/register_repository.dart
@@ -0,0 +1,40 @@
+import 'package:app/app/model/register/register.dart';
+import 'package:app/app/model/remote/base_response.dart';
+import 'package:app/core/remote/core_api.dart';
+import 'package:app/core/remote/endpoints.dart';
+
+abstract class IRegisterRepository {
+  Future<dynamic> registerClient({Register register});
+}
+
+class RegisterRepository implements IRegisterRepository {
+  CoreAPI coreAPI = CoreAPI();
+
+  @override
+  Future<dynamic> registerClient({Register register}) async {
+    try {
+      var body = {
+        "cpf": register.cpf,
+        "nome": register.nome,
+        "telefone": register.telefone,
+        "dataNascimento": register.dataNascimento,
+        "email": register.email,
+        "password": register.password,
+        "admin": false,
+        "cnh": register.cnh,
+        "partner": false
+      };
+      BaseResponseAPI response = await coreAPI.post(
+          body: body, baseUrl: Endpoints.baseURL, endpoint: Endpoints.client);
+
+      if (response.statusCode == 200) {
+        return Register.fromJson(response.response);
+      }
+
+      return response;
+    } catch (e) {
+      return BaseResponseAPI(
+          response: null, statusCode: 500, statusMessage: "error");
+    }
+  }
+}
