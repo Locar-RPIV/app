0e8579e7f0d7d61c96ab60564a9e7e521f0c53cc
Matheus Kildere <matheuskildery94@gmail.com>
2021-06-28 20:07:17 -0300

    feat: partner vehicle

--- lib/app/model/vehicle/partner_vehicle.dart
+++ lib/app/model/vehicle/partner_vehicle.dart
@@ -15,57 +15,58 @@ class PartnerVehicle {
   bool eDeParceiro;
 
   PartnerVehicle(
-      this.id,
-      this.marca,
-      this.modelo,
-      this.potencia,
-      this.placa,
-      this.cor,
-      this.ano,
-      this.tipoCombustivel,
-      this.numeroPortas,
-      this.quilometragem,
-      this.renavan,
-      this.chassis,
-      this.valorLocacao,
-      this.eDeParceiro);
+    this.id,
+    this.marca,
+    this.modelo,
+    this.potencia,
+    this.placa,
+    this.cor,
+    this.ano,
+    this.tipoCombustivel,
+    this.numeroPortas,
+    this.quilometragem,
+    this.renavan,
+    this.chassis,
+    this.valorLocacao,
+    {this.eDeParceiro}
+  );
 
   static List<PartnerVehicle> fromArray(List<dynamic> list) =>
-      list.map((json) => PartnerVehicle.fromJson(json)).toList();
+      list.map((json) => PartnerVehicle.fromJson(json as Map<String, dynamic>)).toList();
 
   PartnerVehicle.fromJson(Map<String, dynamic> map) {
-    id = map['id'];
-    marca = map['marca'];
-    modelo = map['modelo'];
-    potencia = map['potencia'];
-    placa = map['placa'];
-    cor = map['cor'];
-    ano = map['ano'];
-    tipoCombustivel = map['tipoCombustivel'];
-    numeroPortas = map['numeroPortas'];
-    quilometragem = map['quilometragem'];
-    renavan = map['renavan'];
-    chassis = map['chassis'];
-    valorLocacao = map['valorLocacao'];
-    eDeParceiro = map['eDeParceiro'];
+    id = map['id'] as int;
+    marca = map['marca'].toString();
+    modelo = map['modelo'].toString();
+    potencia = map['potencia'] as int;
+    placa = map['placa'].toString();
+    cor = map['cor'].toString();
+    ano = map['ano'] as int;
+    tipoCombustivel = map['tipoCombustivel'] as int;
+    numeroPortas = map['numeroPortas'] as int;
+    quilometragem = map['quilometragem'] as int;
+    renavan = map['renavan'] as int;
+    chassis = map['chassis'].toString();
+    valorLocacao = double.parse(map['valorLocacao'].toString());
+    eDeParceiro = map['eDeParceiro'] as bool;
   }
 
   Map<String, dynamic> toJson() {
-    final Map<String, dynamic> data = new Map<String, dynamic>();
-    data['id'] = this.id;
-    data['marca'] = this.marca;
-    data['modelo'] = this.modelo;
-    data['potencia'] = this.potencia;
-    data['placa'] = this.placa;
-    data['cor'] = this.cor;
-    data['ano'] = this.ano;
-    data['tipoCombustivel'] = this.tipoCombustivel;
-    data['numeroPortas'] = this.numeroPortas;
-    data['quilometragem'] = this.quilometragem;
-    data['renavan'] = this.renavan;
-    data['chassis'] = this.chassis;
-    data['valorLocacao'] = this.valorLocacao;
-    data['eDeParceiro'] = this.eDeParceiro;
+    final Map<String, dynamic> data = <String, dynamic>{};
+    data['id'] = id;
+    data['marca'] = marca;
+    data['modelo'] = modelo;
+    data['potencia'] = potencia;
+    data['placa'] = placa;
+    data['cor'] = cor;
+    data['ano'] = ano;
+    data['tipoCombustivel'] = tipoCombustivel;
+    data['numeroPortas'] = numeroPortas;
+    data['quilometragem'] = quilometragem;
+    data['renavan'] = renavan;
+    data['chassis'] = chassis;
+    data['valorLocacao'] = valorLocacao;
+    data['eDeParceiro'] = eDeParceiro;
     return data;
   }
 }
